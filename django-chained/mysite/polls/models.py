c43ddb5c33dee43cad1d863cdcfb30ed1be4f3d4
21pedrjo <45008235+JoshuaPedro@users.noreply.github.com>
2019-04-26 13:52:57 -0400

    Add files via upload

---
+++ django-chained/mysite/polls/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    #sets max character length of the question to 200 characters     
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    #displays the timezone and the date published
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    #creates the verrible question 
+    def __str__(self):
+        return self.choice_text
